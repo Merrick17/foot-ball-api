224586ed37e74b53d01a670210e555cc02571215
safwen ben fredj <safwen.benfredj@gmail.com>
2023-07-27 22:55:08 +0200

    fixing

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -72,4 +72,79 @@ const getAllUsers = async (req, res) => {
         });
     }
 }
-module.exports = { createNewUser, loginUser, getAllUsers };
+const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { firstName, lastName, password, email, address, dateOfBirth, role } = req.body;
+        
+        // Check if the user exists
+        const user = await User.findById(id);
+        if (!user) {
+            return res.json({ success: false, message: "User not found" });
+        }
+
+        // Update the user properties
+        user.firstName = firstName;
+        user.lastName = lastName;
+        user.address = address;
+        user.dateOfBirth = dateOfBirth;
+        user.role = role;
+
+        // Check if the email is being changed and handle it accordingly
+        if (email && email !== user.email) {
+            const existUser = await User.findOne({ email: email });
+            if (existUser) {
+                return res.json({ success: false, message: "Email already exists" });
+            }
+            user.email = email;
+        }
+
+        // Check if the password is being changed and handle it accordingly
+        if (password) {
+            const hash = await bcrypt.hash(password, 10);
+            user.password = hash;
+        }
+
+        // Save the updated user
+        const result = await user.save();
+
+        res.json({
+            success: true,
+            result: result
+        });
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+// Delete user
+const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        
+        // Check if the user exists
+        const user = await User.findById(id);
+        if (!user) {
+            return res.json({ success: false, message: "User not found" });
+        }
+
+        // Delete the user
+        await user.remove();
+
+        res.json({
+            success: true,
+            message: "User deleted successfully"
+        });
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+module.exports = { createNewUser, loginUser, getAllUsers, updateUser, deleteUser };
+
